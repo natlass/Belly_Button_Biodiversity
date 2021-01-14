e8683a624c5ed1f507f0b4fed122376abba2a2b7
nlass <natlassen@gmail.com>
2021-01-13 23:03:18 -0500

    final

--- static/app.js
+++ static/app.js
@@ -1,21 +1,23 @@
+
+
 function barchart() {
     
     d3.json("/samples.json").then((importedData) => {
         var data = importedData;
-        console.log(data.samples);
+        //console.log(data.samples);
 
     var hovertext = Object.values(data.samples[0].otu_labels);
-    console.log(hovertext);
+    //console.log(hovertext);
 
     
     var label = Object.values(data.samples[0].otu_ids).sort((a, b) => b - a);
     var sliced = label.slice(0, 10);
-    console.log(sliced);
+    //console.log(sliced);
     var arrayofString = sliced.map(otuID => `OTU ${otuID}`);
-    console.log(arrayofString);
+    //console.log(arrayofString);
 
     var value = Object.values(data.samples[0].sample_values);
-    console.log(value);
+    //console.log(value);
 
     var trace0 = [{
       x: value,
@@ -33,17 +35,15 @@ barchart();
 function bubbly() {
     d3.json("/samples.json").then((importedData) => {
         var data = importedData;
-        console.log(data.samples);
+        //console.log(data.samples);
 
     var id = Object.values(data.samples[0].otu_ids)
-    var arrayofString = id.map(otuID => `OTU ${otuID}`);
-    console.log(arrayofString); 
 
     var value = Object.values(data.samples[0].sample_values);
-    console.log(value);
+    //console.log(value);
 
     var hovertext = Object.values(data.samples[0].otu_labels);
-    console.log(hovertext);
+    //console.log(hovertext);
 
     var trace1 = {
         x: id,
@@ -68,3 +68,45 @@ function bubbly() {
     })};
 
 bubbly(); 
+
+function meta(sample) {
+  var metadata = "/metadata/" + sample;
+  //console.log(metadata);
+  var panel = d3.select("#sample-metadata");
+  d3.json(metadata).then(function (data) {
+    Object.entries(data).forEach(([key, value]) => {
+      panel.append("h5").text(`${key}: ${value}`);
+    });
+  });
+}
+
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+
+  // Call updatePlotly() when a change takes place to the DOM
+  var selection = d3.select("#selDataset");
+
+  d3.json('samples.json').then((data)=>{
+    var demoID = data.names;
+    demoID.forEach((sampleNames) => {
+      selection
+        .append('option')
+        .text(sampleNames)
+        .property('value', sampleNames);
+    });
+
+    var firstSample = data[0];
+    barchart(firstSample);
+    bubbly(firstSample);
+    meta(firstSample);
+    //console.log(firstSample);
+  });
+}
+
+function onChange(newSelection) {
+  barchart(newSelection);
+  bubbly(newSelection);
+  meta(newSelection);
+}
+
+updatePlotly();
